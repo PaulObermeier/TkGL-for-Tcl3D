17640030fe046d31d13819b71f99c42b39aa09af
Marc Culler <culler@users.noreply.github.com>
2024-01-21 20:24:26 -0600

    Oh, the problem with cmd may also be that msys is not in the path.

--- ci/windows/build_windows_tkgl.sh
+++ ci/windows/build_windows_tkgl.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
+set -e
 TCL8_VERSION=8.6.13
 TCL9_VERSION=9.0b1
 WGET=/c/msys64/usr/bin/wget
+CMD=/c/msysy64/usr/bin/cmd
 if [ ! -e tcl$TCL8_VERSION-src.tar.gz ] ; then
     $WGET https://prdownloads.sourceforge.net/tcl/tcl$TCL8_VERSION-src.tar.gz
 fi
@@ -24,8 +26,8 @@ mv tcl$TCL9_VERSION tcl9
 tar xfz tk$TCL9_VERSION-src.tar.gz
 mv tk$TCL9_VERSION tk9
 
-#cmd <ci/windows/build_windows_tcltk.bat
-#cmd <ci/windows/build_windows_tkgl.bat
-#rm -rf dist
-#mkdir -p dist/Tkgl1.0
-#mv win/Release*/*.* Tkgl1.0
+$CMD < ci/windows/build_windows_tcltk.bat
+$CMD < ci/windows/build_windows_tkgl.bat
+rm -rf dist
+mkdir -p dist/Tkgl1.0
+mv win/Release*/*.* Tkgl1.0
